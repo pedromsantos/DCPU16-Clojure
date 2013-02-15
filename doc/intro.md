33826f1934525122f69e1a7cb614375a07847894
Pedro Santos <github@mariodelrio.com>
2013-02-15 20:09:38 +0000

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to dcpu16-clojure
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/dcpu16_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns dcpu16-clojure.core-test
+  (:use clojure.test
+        dcpu16-clojure.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
