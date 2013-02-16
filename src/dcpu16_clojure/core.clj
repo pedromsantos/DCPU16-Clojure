1d57cb74cdd8254493d0c771700417ed3a142d7d
Pedro Santos <github@mariodelrio.com>
2013-02-16 15:06:24 +0000

    Added defines for regular expressions.

--- src/dcpu16_clojure/core.clj
+++ src/dcpu16_clojure/core.clj
@@ -1,20 +1,34 @@
 (ns dcpu16-clojure.core)
 
+(def re-close-bracket #"^[\]\)]")
+(def re-comma #"^,")
+(def re-comment #"^;.*$")
+(def re-decimal #"^[0-9]+")
+(def re-hexadecimal #"^0(?i)x[0-9a-fA-F]+")
+(def re-instruction #"^dat|^set|^add|^sub|^mul|^div|^mod|^shl|^shr|^and|^bor|^xor|^ife|^ifn|^ifg|^ifb|^jsr")
+(def re-label-ref #"^[a-zA-Z0-9_]+")
+(def re-label #"^:\w+")
+(def re-open-bracket #"^[\[\(]")
+(def re-plus #"^\+")
+(def re-register #"^a|^b|^c|^x|^y|^z|^i|^j|^pop|^push|^peek|^pc|^sp|^o")
+(def re-string #"^\"(\"|[^\"])*\"")
+(def re-whitespace #"^(\r\n|\s+)")
+
 (def matchers
        {
-                 :close-bracket #"^[\]\)]",
-                 :comma #"^,",
-                 :comment #"^;.*$",
-                 :decimal #"^[0-9]+",
-                 :hexadecimal #"^0(?i)x[0-9a-fA-F]+",
-                 :instruction #"^(((?i)dat)|((?i)set)|((?i)add)|((?i)sub)|((?i)mul)|((?i)div)|((?i)mod)|((?i)shl)|((?i)shr)|((?i)and)|((?i)bor)|((?i)xor)|((?i)ife)|((?i)ifn)|((?i)ifg)|((?i)ifb)|((?i)jsr))",
-                 :label-ref #"^[a-zA-Z0-9_]+"
-                 :label #"^:\w+",
-                 :open-bracket #"^[\[\(]",
-                 :plus #"^\+"'
-                 :register #"\b(((?i)a)|((?i)b)|((?i)c)|((?i)x)|((?i)y)|((?i)z)|((?i)i)|((?i)j)|((?i)pop)|((?i)push)|((?i)peek)|((?i)pc)|((?i)sp)|((?i)o))\b",
-                 :string #"\"(\"|[^\"])*\"",
-                 :whitespace #"^(\r\n|\s+)"
+                 :close-bracket re-close-bracket,
+                 :comma re-comma,
+                 :comment re-comment,
+                 :decimal re-decimal,
+                 :hexadecimal re-hexadecimal,
+                 :instruction re-instruction,
+                 :label-ref re-label-ref,
+                 :label re-label, 
+                 :open-bracket re-open-bracket,
+                 :plus re-plus,
+                 :register re-register,
+                 :string re-string,
+                 :whitespace re-whitespace,
               })
  
 (re-seq (matchers :close-bracket) "] ")
@@ -23,8 +37,8 @@
 (re-seq (matchers :decimal) "10 ")
 (re-seq (matchers :hexadecimal) "0x01 ")
 (re-seq (matchers :instruction) "set ")
-(re-seq (matchers :instruction) "SET ")
-(re-seq (matchers :instruction) "Set ")
+(re-seq (matchers :instruction) "IFE ")
+(re-seq (matchers :instruction) "o jsr ")
 (re-seq (matchers :label-ref) "label ")
 (re-seq (matchers :label) ":label ")
 (re-seq (matchers :open-bracket) "[ ")
@@ -35,4 +49,5 @@
 (re-seq (matchers :register) "x ")
 (re-seq (matchers :register) "J ")
 (re-seq (matchers :string) "\"label\" ")
+
 
